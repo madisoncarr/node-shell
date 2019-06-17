91e28abb4ee0d526ea6f56de9a667a5d96822005
Vera <vera.krutsina@gmail.com>
2019-06-17 12:04:32 -0500

    pwd and ls completed

--- bash.js
+++ bash.js
@@ -1,15 +1,14 @@
+const pwd = require('./pwd');
+const ls = require('./ls');
+
 process.stdout.write('prompt >');
 
-process.stdin.on('data', (data) => {
-	const cmd = data.toString().trim();
-
-	process.stdout.write('You typed: ' + cmd);
-	process.stdout.write('\nprompt > ');
-});
-
-process.stdin.on('data', (data) => {
-	const pwd = data.toString().trim();
-
-	process.stdout.write(process.cwd());
-	process.stdout.write('\nprompt >');
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
+  process.stdout.write('You typed: ' + cmd + '\n');
+  if (cmd === 'ls') {
+    ls();
+  } else if (cmd === 'pwd') {
+    pwd();
+  }
 });
